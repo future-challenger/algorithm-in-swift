27ccd81d9fb1f606692178cc10e39f545f5f1193
Uncle Charlie <lixiangdong@mobike.com>
2017-10-31 10:18:38 +0800

    first commit

---
+++ Algorithm-in-Swift/Algorithm-in-Swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Algorithm-in-Swift
+//
+//  Created by Uncle Charlie on 31/10/2017.
+//  Copyright © 2017 Uncle Charlie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
